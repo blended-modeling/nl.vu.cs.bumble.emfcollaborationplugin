a2194dc05b5d7c425d5c13d291ae834b42fd8216
Yuna C <yunabell@outlook.com>
2023-03-08 11:15:20 +0100

    add replace reference support

--- src/nl/vu/cs/bumble/emfcollaborationplugin/handlers/Value.java
+++ src/nl/vu/cs/bumble/emfcollaborationplugin/handlers/Value.java
@@ -2,6 +2,7 @@ package nl.vu.cs.bumble.emfcollaborationplugin.handlers;
 
 public class Value {
 	private String $type;
+	private String $ref;
 	
 	public Value() {
 	};
@@ -10,7 +11,15 @@ public class Value {
 		return this.$type;
 	}
 	
+	public String get$ref() {
+		return this.$ref;
+	}
+	
 	public void setType(String value) {
 		this.$type = value;
+	}
+	
+	public void setRef(String value) {
+		this.$ref = value;
 	}
 }
